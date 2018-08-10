e9c1d9e510e72ee35daef42b91a37b9921c24a55
Anuj Katiyar <katiyaranuj@icloud.com>
2018-08-11 00:27:01 +0530

    Created Employee Model getter setter

--- SimpleRestApplication/src/main/java/com/model/Employee.java
+++ SimpleRestApplication/src/main/java/com/model/Employee.java
@@ -1,5 +1,53 @@
 package com.model;
 
 public class Employee {
+	
+	private String firstName;
+	private String lastName;
+	private int employeeId;
+	private String designation;
+	private String department;
+	private String address;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
 
 }
